aadd33b7a82613521fe899e9760291fe83c87b09
Caitlin Lee <caitlinlee2000@gmail.com>
2021-11-07 01:27:22 -0800

    added sw.js functionality

--- sw.js
+++ sw.js
@@ -9,7 +9,23 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
-  
+  let urlsToCache = [
+    'https://introweb.tech/assets/json/ghostCookies.json',
+    'https://introweb.tech/assets/json/birthdayCake.json',
+    'https://introweb.tech/assets/json/chocolateChip.json',
+    'https://introweb.tech/assets/json/stuffing.json',
+    'https://introweb.tech/assets/json/turkey.json',
+    'https://introweb.tech/assets/json/pumpkinPie.json'
+  ]; 
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache){
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+
+
 });
 
 /**
@@ -22,6 +38,8 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+  
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -30,4 +48,15 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+   event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    ) 
+  );
 });
